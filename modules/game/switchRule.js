7f032e7706518e0a190106836c58b8f28a7e7fa2
dh <sishen753951@qq.com>
2022-05-04 12:36:50 +0800

    feat: 新增修改设置时显示提示信息

--- modules/game/switchRule.js
+++ modules/game/switchRule.js
@@ -6,7 +6,7 @@ import _ from '@/modules'
  */
 const switchRule = () => {
   const { data } = _
-  data.ruleIndex = (data.ruleIndex + 1) % 3
+  data.ruleIndex = (data.ruleIndex + 1) % 4
   data.showRuleTip = false
 }
 
--- modules/setting/changeSetting.js
+++ modules/setting/changeSetting.js
@@ -6,13 +6,20 @@ import _ from '@/modules'
  * @return {void}
  */
 const changeSetting = (prop) => {
-  const { setting } = _
+  const { setting, showTip } = _
   if (prop == 'life') {
     setting.infiniteLife = !setting.infiniteLife
+    showTip(`无限生命功能已${setting.infiniteLife ? '开启' : '关闭'}`, 'none')
   } else if (prop == 'sort') {
     setting.sort = !setting.sort
+    if (setting.sort) {
+      showTip('你将依次面对 J Q K', 'none')
+    } else {
+      showTip('你将面对随机的敌人', 'none')
+    }
   } else if (prop == 'honor') {
     setting.honor = !setting.honor
+    showTip(`荣誉击杀功能已${setting.honor ? '开启' : '关闭'}`, 'none')
   }
 }
 
